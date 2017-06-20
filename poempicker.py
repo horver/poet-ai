4c3d87d770641e72b30f1192e28d12d8ad433b53
Frekimoto <tamasurfi@gmail.com>
2017-06-20 17:01:49 +0200

    + Bootstrap
    + HTML template

--- poempicker.py
+++ poempicker.py
@@ -1,8 +1,26 @@
 import urllib.request
 import bs4
+import markovify
+
+urls = [
+    'http://mek.oszk.hu/01000/01006/html/vs184602.htm',
+    'http://mek.oszk.hu/00500/00588/html/vers1401.htm',
+    'http://mek.oszk.hu/06200/06266/html/tragedia0002.html'
+]
 
 def get_text_url(url):
     return bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser").find("body").get_text()
 
 def get_poems(raw, delimiter = "\n\n\n\xa0\n\n\n\n"):
     return raw.split(delimiter)
+
+def generate_poems(text, number = 10):
+    text = text.replace('\r\n', '<br>')
+    model = markovify.Text(text)
+    content = '<div>'
+    for i in range(number):
+        sentence = str(model.make_sentence())
+        if (sentence != 'None'):
+            content += sentence
+    content += '</div>'
+    return content
--- views.py
+++ views.py
@@ -1,15 +1,10 @@
+from flask import render_template
 from app import app
-from poempicker import *
-import markovify
+import poempicker
 
 @app.route('/')
 def index():
-    text = get_text_url('http://mek.oszk.hu/01000/01006/html/vs184602.htm')
-    text.replace('\n', '<br>')
-    model = markovify.Text(text)
-    content = ''
-    for i in range(10):
-        sentence = str(model.make_sentence())
-        if (sentence != 'None'):
-            content += sentence
-    return content
+    poems = poempicker.generate_poems(poempicker.get_text_url(poempicker.urls[0]))
+    return render_template('index.html',
+                            urls = poempicker.urls,
+                            content = poems)
