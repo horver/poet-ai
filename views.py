3e830f200c3d62aa0fd0460eee696682617d86bf
Frekimoto <tamasurfi@gmail.com>
2017-06-20 12:32:45 +0200

    + Markov chain

--- views.py
+++ views.py
@@ -1,5 +1,15 @@
 from app import app
+from poempicker import *
+import markovify
 
 @app.route('/')
 def index():
-    return '<h1>hello world</h1>'
+    text = get_text_url('http://mek.oszk.hu/01000/01006/html/vs184602.htm')
+    text.replace('\n', '<br>')
+    model = markovify.Text(text)
+    content = ''
+    for i in range(10):
+        sentence = str(model.make_sentence())
+        if (sentence != 'None'):
+            content += sentence
+    return content
